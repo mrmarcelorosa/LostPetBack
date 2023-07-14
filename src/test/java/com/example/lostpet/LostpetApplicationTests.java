b13c73873dd70f70170fb457c06d6a4140d2ec5e
Marcelo Rosa <vyctor.rosa14@gmail.com>
2023-07-14 12:58:35 -0300

    first commit

---
+++ src/test/java/com/example/lostpet/LostpetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lostpet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostpetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
